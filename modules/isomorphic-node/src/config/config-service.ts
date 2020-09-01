401978fd44c209d0289fbb26a1c422c1206de759
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-09-01 18:55:23 +0200

    WIP

---
+++ modules/isomorphic-node/src/config/config-service.ts
@@ -0,0 +1,17 @@
+import { IConfigService } from "../../config";
+import { mkBytes32, mkPublicIdentifier, mkAddress } from "../utils";
+
+export class MockConfigService implements IConfigService {
+  getPrivateKey(): string {
+    return mkBytes32("0xa");
+  }
+  getPublicIdentifer(): string {
+    return mkPublicIdentifier("indrande");
+  }
+  getSignerAddress(): string {
+    return mkAddress("0xabcde");
+  }
+  getMessagingUrl(): string {
+    return "http://localhost:4222";
+  }
+}
